dcc0402af8fdf90e91e78d991c65333bc51d428c
Pol Timmer <pol.timmer@gmail.com>
2018-05-09 16:47:10 +0200

    added student ID

--- src/nl/tue/algorithms/dbl/wholeFile/PackingSolver.java
+++ src/nl/tue/algorithms/dbl/wholeFile/PackingSolver.java
@@ -11,17 +11,11 @@ import nl.tue.algorithms.dbl.algorithm.FirstFitDecreasingHeight;
  * @author E.M.A. Arts (1004076)
  * @author T.M. Verberk (1016472)
  * @author Koen (1018025)
-<<<<<<< HEAD
- * @author Wouter ()
- * @author Pol (1007701)
- * @author Robin ()
-=======
  * @author Wouter (1009509)
- * @author Pol ()
+ * @author Pol (1007701)
  * @author Robin (1011291)
  * 
  * @since 9 MAY 2018
->>>>>>> 06169a534044dc15c07c54d56d1926b449e88469
  */
 public class PackingSolver {
     public static void main(String args[]) {
