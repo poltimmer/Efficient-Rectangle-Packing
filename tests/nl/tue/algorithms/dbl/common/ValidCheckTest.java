a7540bca441c807e90bffea29807d095bbbd456f
s168227 <kees.voorintholt@live.nl>
2018-05-16 15:49:38 +0200

    Added test file for ValidCheck

---
+++ tests/nl/tue/algorithms/dbl/common/ValidCheckTest.java
@@ -0,0 +1,28 @@
+package nl.tue.algorithms.dbl.common;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author K.D. Voorintholt (1005136)
+ *
+ * @since 9 MAY 2018
+ */
+public class ValidCheckTest extends TestCase {
+    public void testCheckSolution() throws Exception {
+
+    }
+
+    public void testNoOverlapSolution() throws Exception {
+
+    }
+
+    public void testFitsInContainer() throws Exception {
+
+    }
+
+    public void testNoIllegalRotation() throws Exception {
+
+    }
+
+}
