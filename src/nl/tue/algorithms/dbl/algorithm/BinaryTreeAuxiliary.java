f13eeaf96aa8f6d433ded3ab5fda8d6e2d087fe9
s165318 <r.m.jonker@student.tue.nl>
2018-05-24 16:26:05 +0200

    Improved growNode auxiliary function
    growNode now throws an error when it should, contracts worked out as
    well.

--- src/nl/tue/algorithms/dbl/algorithm/BinaryTreeAuxiliary.java
+++ src/nl/tue/algorithms/dbl/algorithm/BinaryTreeAuxiliary.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.tue.algorithms.dbl.algorithm;
-
 
 import nl.tue.algorithms.dbl.common.Node;
 
@@ -16,19 +10,29 @@ import nl.tue.algorithms.dbl.common.Node;
 public class BinaryTreeAuxiliary {
     
     /**
-     * Call this function with the width and height of the to be placed rectangle
-     * to see if it should be placed up or right of the node.
-     * Only useful if there is one node, and defaults to the right if right and up are the same.
+     * Decides where the algorithm should grow to (right or up).
+     * Only works in the case of one node
      * 
+     * 
+     * @param node the one node available
+     * @param w the width of the rectangle to be placed
+     * @param h the height of the rectangle to be placed
+     * 
+     * @throws IllegalStateException if both canGrowUp and canGrowRight are false
+     * 
+     * @return true if algo should grow up, false if algo should grow right
      */
-    public static boolean growNode(Node node, int w, int h) {
+    public static boolean growNode(Node node, int w, int h) throws IllegalStateException {
         boolean canGrowUp  = (w <= node.getxNode());
         boolean canGrowRight = (h <= node.getyNode());
 
         boolean shouldGrowRight = canGrowRight && (node.getyNode() >= (node.getxNode() + w)); // attempt to keep square-ish by growing right when height is much greater than width
         boolean shouldGrowUp  = canGrowUp  && (node.getxNode() >= (node.getyNode() + h)); // attempt to keep square-ish by growing down  when width  is much greater than height
 
-        if (shouldGrowRight) {
+        if (!(canGrowRight || canGrowUp)) {
+            throw new IllegalStateException("Rectangle can't be placed, make sure to sort the list of rectangles on width or height");
+        }
+        else if (shouldGrowRight) {
             return false;
         }
         else if (shouldGrowUp) {
@@ -37,13 +41,8 @@ public class BinaryTreeAuxiliary {
         else if (canGrowRight) {
             return false;
         }
-        else if (canGrowUp) {
+        else {
             return true;
         }
-        else {
-            // Throw errorS
-            // still should never happen if the rectangles are sorted in advance
-        }
-        return false; //defaults to false, but this should never happen once the error is created
     }
 }
