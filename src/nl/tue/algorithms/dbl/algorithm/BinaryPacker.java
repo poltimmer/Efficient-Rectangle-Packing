bcc2cbf7e8813209d426f2c929ef43d152758f2a
s165318 <r.m.jonker@student.tue.nl>
2018-06-06 17:10:45 +0200

    BinaryPacker does not recurse anymore, resolving stackoverflow error

--- src/nl/tue/algorithms/dbl/algorithm/BinaryPacker.java
+++ src/nl/tue/algorithms/dbl/algorithm/BinaryPacker.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public class BinaryPacker extends  Algorithm<PackHeightQueue> {
 
-    private ArrayList<Node> nodeList = new ArrayList<>();
+    private final ArrayList<Node> nodeList = new ArrayList<>();
     private Node rightNode;
     private Node topNode;
 
@@ -31,10 +31,12 @@ public class BinaryPacker extends  Algorithm<PackHeightQueue> {
             nodeList.add(node);
             rightNode = node;
             topNode = node;
-            placeRectangle(topNode, rightNode, nodeList);
+            //placeRectangle(topNode, rightNode, nodeList);
+            placeAllRectangles();
         }
     }
 
+    /*
     private void placeRectangle(Node top, Node right, ArrayList<Node> list) {
         nodeList = list;
         topNode = top;
@@ -103,6 +105,7 @@ public class BinaryPacker extends  Algorithm<PackHeightQueue> {
             ValidCheck.print("done");
         }
     }
+    */
 
     private void placeOneNode(RectangleRotatable r) {
         //If one node, just place it to the right or top
@@ -128,5 +131,72 @@ public class BinaryPacker extends  Algorithm<PackHeightQueue> {
             nodeList.add(x);
         }
         return;
+    }
+    
+    private void placeAllRectangles() {
+        for (int rectanglesPlaced = 1; rectanglesPlaced <= pack.getNumberOfRectangles(); rectanglesPlaced++ ) {
+            if (!pack.getRectangles().isEmpty()) {
+                RectangleRotatable r = pack.getRectangles().poll();
+                //ValidCheck.print(r.getRotatedHeight());
+                if (nodeList.size() == 1) {
+                    placeOneNode(r);
+                    //placeRectangle(topNode, rightNode, nodeList);
+                }  else {
+                    //if more than one node, fill in the gap.
+                    for (Node n : nodeList) {
+                        if (n != rightNode) {
+                            if (BinaryTreeAuxiliary.fits(n, r) && r.getRotatedWidth() + n.getxNode() <= rightNode.getxNode()) { //if rectangle fits in node and does not extend the right most node
+                                r.setLocation(n.getxNode(), n.getyNode() - n.getyRoom()); //place node
+                                n.setyRoom(n.getyRoom() - r.getRotatedHeight()); //change the values of the node
+                                Node x = new Node(r.x + r.getRotatedWidth(), r.y + r.getRotatedHeight(), r.getRotatedWidth(), r.getRotatedHeight()); //adds new node
+                                //removes node if node is not needed anymore
+                                if (n.getyRoom() == 0) {
+                                    x.setxRoom(n.getxRoom() + x.getxRoom());
+                                    if (n == topNode && n == rightNode) {
+                                        topNode = x;
+                                        rightNode = x;
+                                    } else if (n == rightNode) {
+                                        rightNode = x;
+                                    } else if (n == topNode) {
+                                        topNode = x;
+                                    }
+                                    nodeList.remove(n);
+                                }
+                                nodeList.add(x);
+                                break;
+                            }
+                        }
+                    }
+                    //if the rectangle couldnt be placed right of a node (except of the rightmost node)
+                    if (r.y <= -1 || r.x <= -1) {
+                        //plaatsen bij de right node of top node
+                        //If true then grow up, if false grow right
+                        if (BinaryTreeAuxiliary.growTwoNodes(topNode, rightNode, r)) {
+                            r.setLocation(0, topNode.getyNode());
+                            Node x = new Node(r.x + r.getRotatedWidth(), r.y + r.getRotatedHeight(), r.getRotatedWidth(), r.getRotatedHeight());
+                            topNode = x;
+                            nodeList.add(x);
+                            //placeRectangle(topNode, rightNode, nodeList);
+                        } else {
+                            r.setLocation(rightNode.getxNode(), 0);
+                            Node x = new Node(r.x + r.getRotatedWidth(), r.y + r.getRotatedHeight(), r.getRotatedWidth(), r.getRotatedHeight());
+                            for (Node n : nodeList) {
+                                if (n == rightNode) {
+                                    n.setyRoom(rightNode.getyRoom() - r.getRotatedHeight());
+                                }
+                            }
+                            rightNode = x;
+                            nodeList.add(x);
+                            //placeRectangle(topNode, rightNode, nodeList);
+                        }
+
+                    } else {
+                        //placeRectangle(topNode, rightNode, nodeList);
+                    }
+                }
+            } else {
+                ValidCheck.print("done");
+            }
+        }
     }
 }
